71d6f20d27f3af8a39e20089c0c370fd90502978
Vijay Singh <thakurvijayofficial@gmail.com>
2025-04-22 08:07:56 +0530

    Changed CORS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://pnr-frontend-nu.vercel.app/"],
+    origin: ["http://localhost:5173", "https://pnr-frontend-nu.vercel.app"],
   })
 );
 
