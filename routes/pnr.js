c7e9174fd97746bdee11f522744cf0521da82288
Vijay Singh <thakurvijayofficial@gmail.com>
2025-04-30 20:05:05 +0530

    Showing saved captcha

--- routes/pnr.js
+++ routes/pnr.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 const { fetchPNRDetail } = require("../controller/pnr");
 router.get("/pnr/:pnr", fetchPNRDetail);
-
+router.get("/captcha", (req, res) => {
+  const imgPath = path.join(__dirname, "..", "captcha.jpg");
+  return res.sendFile(imgPath);
+});
 module.exports = router;
